e9dce166056ed4db507a9472ddb50bafe71decae
Levi Durfee <levi.durfee@gmail.com>
2017-11-17 20:34:34 -0500

    add exceptions [#5]

--- src/Providers/UserProvider.php
+++ src/Providers/UserProvider.php
@@ -4,12 +4,18 @@ use wappr\Models\User as UserModel;
 use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
 class UserProvider implements UserProviderInterface
 {
     public function loadUserByUsername($username)
     {
         $user = UserModel::where('username', $username)->get();
+
+        if($user->isEmpty()) {
+            throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
+        }
 
         return new User($user->username, $user->password, 'ROLE_ADMIN');
     }
